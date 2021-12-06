821eed9b23038bce62792340c5c88a353b42da51
Simran Singhi <ssinghi145@gmail.com>
2021-12-06 03:11:57 -0600

    Change in app.py

--- app.py
+++ app.py
@@ -339,4 +339,4 @@ def delete_article(id):
 
 if __name__ == '__main__':
     app.secret_key='secret123'
-    app.run(debug=True, port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
